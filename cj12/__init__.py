9fd4d4e54eb41d1d6cdd6a756efc2e0bf62204f4
interrrp <interrrp@proton.me>
2025-08-08 22:14:33 +0800

    Simplify module loading

---
+++ cj12/__init__.py
@@ -0,0 +1,5 @@
+from cj12.arithmetic import add
+
+
+def start() -> None:
+    print(add(2, 2))  # noqa: T201
--- cj12/__main__.py
+++
@@ -1,9 +0,0 @@
-from cj12.arithmetic import add
-
-
-def main() -> None:
-    print(add(2, 2))  # noqa: T201
-
-
-if __name__ == "__main__":
-    main()
