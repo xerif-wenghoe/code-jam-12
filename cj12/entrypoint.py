7266307a9377043beb596edb62ff639661ecd0b1
interrrp <interrrp@proton.me>
2025-08-10 21:04:52 +0800

    Refactor

--- cj12/entrypoint.py
+++ cj12/entrypoint.py
@@ -5,6 +5,6 @@ from pyodide.http import pyfetch
 async def start() -> None:
     document.title = "Code Jam 12"
 
-    resp = await pyfetch("/cj12/ui.html")
+    resp = await pyfetch("/ui.html")
     html = await resp.text()
     document.body.innerHTML = html
--- server.py
+++ server.py
@@ -1,19 +1,12 @@
 import uvicorn
 from starlette.applications import Starlette
-from starlette.requests import Request
-from starlette.responses import FileResponse
-from starlette.routing import Mount, Route
+from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
-
-
-async def serve_index(_: Request) -> FileResponse:
-    return FileResponse("cj12/index.html")
-
 
 app = Starlette(
     routes=[
-        Route("/", serve_index),
         Mount("/cj12", StaticFiles(directory="cj12")),
+        Mount("/", StaticFiles(directory="static", html=True)),
     ],
 )
 
