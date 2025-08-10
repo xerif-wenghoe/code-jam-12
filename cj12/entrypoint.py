22be64846345dc19d27b7a9431667ca718ea59f4
interrrp <interrrp@proton.me>
2025-08-10 20:48:26 +0800

    Move to separate HTML file

--- cj12/entrypoint.py
+++ cj12/entrypoint.py
@@ -1,34 +1,10 @@
 from js import document
+from pyodide.http import pyfetch
 
 
 async def start() -> None:
     document.title = "Code Jam 12"
-    document.body.innerHTML = """
 
-<div class="main">
-
-  <div class="section">
-    <h1 class="title">Super Ultra Encryptor</h1>
-  </div>
-
-  <div class="section">
-    <label for="fileInput" class="dropzone">Drag & Drop files here or click to select</label>
-    <input type="file" id="fileInput" style="display:none" />
-  </div>
-
-  <div class="section">
-    <div class="selections">
-      <img src="assets/images/p1.jpg" class="image-placeholder">
-      <img src="assets/images/p1.jpg" class="image-placeholder">
-      <img src="assets/images/p1.jpg" class="image-placeholder">
-      <img src="assets/images/p1.jpg" class="image-placeholder">
-      <img src="assets/images/p1.jpg" class="image-placeholder">
-    </div>
-  </div>
-
-  <div class="section">
-  </div>
-
-</div>
-
-"""
+    resp = await pyfetch("/cj12/ui.html")
+    html = await resp.text()
+    document.body.innerHTML = html
