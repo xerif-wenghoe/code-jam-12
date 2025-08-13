c75286ed304681bf94984d0cf1836a9d0eb9ec51
Ryan T <ryan.truong.nsw@gmail.com>
2025-08-13 10:43:57 +1000

    file info displays when selected, idk where it should go

--- cj12/app.py
+++ cj12/app.py
@@ -22,15 +22,32 @@ class App:
     def _register_file_input_handler(self) -> None:
         @create_proxy
         def on_input_element_change(event: object) -> None:
+            
+            file = event.target.files.item(0)
+            document.getElementById("file-area").innerHTML = f'''
+            <section id="file-display">
+            <div class="inner-flex">
+                <p>{file.name}</p>
+                <p>{round(file.size/1024, 2)} KB</p>
+            </div>
+            <div class="inner-flex">
+                <button id="encrypt-button" class="right" disabled>Encrypt</button>
+                <button id="decrypt-button" class="right" disabled>Decrypt</button>
+            </div>
+            </section>
+            '''
+
             reader = FileReader.new()
             reader.addEventListener("load", on_content_load)
-            reader.readAsBinaryString(event.target.files.item(0))  # pyright: ignore[reportUnknownMemberType, reportUnknownArgumentType, reportAttributeAccessIssue]
+            reader.readAsBinaryString(file)  # pyright: ignore[reportUnknownMemberType, reportUnknownArgumentType, reportAttributeAccessIssue]
+
 
         @create_proxy
         def on_content_load(event: object) -> None:
             self._data = event.target.result.encode()  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
             get_element_by_id("encrypt-button", ButtonElement).disabled = False
             get_element_by_id("decrypt-button", ButtonElement).disabled = False
+
 
         file_input = get_element_by_id("file-input")
         file_input.addEventListener("change", on_input_element_change)
