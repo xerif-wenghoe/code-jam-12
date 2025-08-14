584403ae123ec8f355f9b9bfadd1b6f295e82871
Lawrence Feng <spnether1411@gmail.com>
2025-08-14 17:46:06 +0700

    feat: implement encrypt/decrypt with key and download

--- cj12/entrypoint.py
+++ cj12/entrypoint.py
@@ -1,10 +1,12 @@
-import base64
+import logging
 
 from js import document
 from pyodide.http import pyfetch
 
 
 async def start() -> None:
+    logging.basicConfig(level=logging.INFO)
+
     document.title = "Code Jam 12"
 
     resp = await pyfetch("/ui.html")
