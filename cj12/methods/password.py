def487f941b78f98f51e4be53ffa958339feb7b5
max-min-median <152759160+max-min-median@users.noreply.github.com>
2025-08-18 22:16:39 +0800

    Password lock now checks for mismatch

--- cj12/methods/password.py
+++ cj12/methods/password.py
@@ -12,10 +12,23 @@ class PasswordMethod:
 
     on_key_received: KeyReceiveCallback | None = None
 
+
     async def setup(self) -> None:
-        self._input = elem_by_id("password-input", InputElement)
-        add_event_listener(self._input, "keydown", self._on_key_down)
+        self._input = [None] * 2
+        self._warn_mismatch = elem_by_id("warn-mismatch")
+        for i in range(2):
+            self._input[i] = elem_by_id(f"password-input{i}", InputElement)
+            add_event_listener(self._input[i], "keydown", self._on_key_down)
+
 
     async def _on_key_down(self, event: Any) -> None:
-        if self.on_key_received is not None and event.key == "Enter":
-            await self.on_key_received(self._input.value.encode())
+        if self.on_key_received is None:
+            return
+        if self._input[0].value != self._input[1].value:
+            self._warn_mismatch.style.color = "#FF0000"
+            self._warn_mismatch.innerText = "Passwords don't match!"
+            await self.on_key_received(None)
+        else:
+            self._warn_mismatch.style.color = "#00FF00"
+            self._warn_mismatch.innerText = "- OK -"
+            await self.on_key_received(self._input[0].value.encode())
